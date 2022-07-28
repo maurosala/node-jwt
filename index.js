25cc6ba987986235f81df7c55dddc100c7304217
mauro sala <mauro.sala@gmail.com>
2022-07-28 14:58:08 +0200

    bearer cookie

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express')
 const server = express()
 const port = 8080
+const jwt = require('jsonwebtoken')
+
+const secret = '5RBW0rDp78c5j4BKOq8lLN0eZ27HjGip'
+const issuer = 'Rmqi7mOBWHuWmIxKmynIVZU3mC5OTGQM'
 
 server.get('/', (req, res) => {
-  console.log(req.headers)
-  res.status(200).json(req.headers)
+  if (process.env.DEBUG) {
+    return res.status(200).json(req.headers)
+  }
+
+  let b = null
+  if (req.headers.authorization) {
+    b = req.headers.authorization.split(' ')[1]
+  }
+  if (req.headers.cookie) {
+    b = req.headers.cookie.split(';')[0].split('=')[1]
+  }
+
+  if (b) {
+    jwt.verify(b, secret, { issuer }, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Invalid token' })
+      }
+
+      return res.status(200).json(decoded)
+    })
+  } else {
+    res.status(406).json({ error: 'No token' })
+  }
 })
 
 server.listen(port, () => {
--- token.js
+++ token.js
@@ -4,11 +4,15 @@ const axios = require('axios')
 const secret = '5RBW0rDp78c5j4BKOq8lLN0eZ27HjGip'
 const issuer = 'Rmqi7mOBWHuWmIxKmynIVZU3mC5OTGQM'
 
+axios.defaults.withCredentials = true
+
 const main = async () => {
-  const t = jwt.sign({ id: 'mauro' }, secret, {
-    issuer,
-    expiresIn: '1h'
-  })
+  // const t = jwt.sign({ id: 'mauro' }, secret, {
+  //   issuer,
+  //   expiresIn: '1h'
+  // })
+  const t =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im1hdXJvIiwiaWF0IjoxNjU5MDEyMDc4LCJleHAiOjE2NTkwMTU2NzgsImlzcyI6IlJtcWk3bU9CV0h1V21JeEtteW5JVlpVM21DNU9UR1FNIn0.6EgpWIU2iKQgkNce1gpDvEXtkklvNIh-S0Gt9FnbTmI'
 
   console.log(t)
 
@@ -16,14 +20,15 @@ const main = async () => {
   axios
     .get(api, {
       headers: {
-        Authorization: `Bearer ${t}`
+        Cookie: `krateo=${t}`
+        // Authorization: `Bearer ${t}`
       }
     })
     .then((res) => {
       console.log(res.data)
     })
     .catch((err) => {
-      console.log(err)
+      console.log(err.response.data)
     })
 }
 
