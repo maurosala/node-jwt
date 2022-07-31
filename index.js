5486772e47611762df475ea8f65c6134613c4d8e
mauro sala <mauro.sala@gmail.com>
2022-07-31 10:25:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ server.get('/', (req, res) => {
 })
 
 server.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
